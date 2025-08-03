0a005e5610c67c3960f9babe3cd8b78690fba3b4
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-08-04 00:17:33 +0300

    Added Context Api for global state management

---
+++ alx-project-0x05/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "@/components/styles/globals.css";
+import Layout from "@/components/layout/Layout";
+import type { AppProps } from "next/app";
+import { CountProvider } from "@/context/CountContext";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
+}
