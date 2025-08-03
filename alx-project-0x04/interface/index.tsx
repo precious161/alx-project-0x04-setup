fd15a34b2808c44e374b87a8016cb6cb12dbd620
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-08-03 23:04:01 +0300

    Intial commit of the project

---
+++ alx-project-0x04/interface/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+export interface LayoutProps {
+  children: ReactNode;
+}
+export interface PageRouteProps {
+  pageRoute: string;
+}
